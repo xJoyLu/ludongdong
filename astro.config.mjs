2487b42384123713c785bc023442934584f1b989
卢东东 <xjoylu@gmail.com>
2024-09-21 18:11:58 +0800

    1
    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,44 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [starlight({
+        title: {
+            zh: '卢东东工作室',
+            en: 'LDD-Studio',
+        },
+        logo: {
+            src: './src/assets/logo.svg',
+          },
+        social: {
+                twitter: 'https://x.com/xjoylu',
+                youtube: 'https://www.youtube.com/@xJoyLu',
+        },
+        sidebar: [
+            {
+                label: 'Guides',
+                items: [
+                    // Each item here is one entry in the navigation menu.
+                    { label: 'Example Guide', slug: 'guides/example' },
+                ],
+            },
+            {
+                label: 'Reference',
+                autogenerate: { directory: 'reference' },
+            },
+        ],
+        customCss: [
+            // 你的自定义 CSS 文件的相对路径
+            './src/css/style.css',
+			// 你的 Tailwind 基础样式的相对路径
+			'./src/css/tailwind.css',
+          ],
+		}), tailwind({
+			// 禁用默认的基础样式
+			applyBaseStyles: false,
+		  })],
+});
---
+++ src/content/docs/guides/hello.md
@@ -0,0 +1,6 @@
+---
+title: Hello, World!
+description: 这是我的 Starlight 网站中的一个页面
+---
+
+第一个页面
