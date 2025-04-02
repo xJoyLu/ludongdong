83b9744617809fb210179538c59d249aa8df1476
卢东东 <xjoylu@gmail.com>
2025-04-03 03:37:11 +0800

    测试更新

---
+++ erudite-diy/src/consts.ts
@@ -0,0 +1,35 @@
+import type { IconMap, SocialLink, Site } from '@/types'
+
+export const SITE: Site = {
+  title: '卢东东',
+  description:
+    '偷懒界翘楚',
+  href: 'https://ldd.cc',
+  featuredPostCount: 2,
+  postsPerPage: 3,
+}
+
+export const NAV_LINKS: SocialLink[] = [
+  { href: '/blog', label: '博客' },
+  { href: '/authors', label: '作者' },
+  { href: '/tags', label: '标签' },
+  { href: '/about', label: '关于' },
+  { href: '/contact', label: '联系' },
+  { href: '/now', label: '现在' }, 
+]
+
+export const SOCIAL_LINKS: SocialLink[] = [
+  { href: 'https://github.com/xjoylu', label: 'GitHub' },
+  { href: 'https://x.com/xjoylu', label: 'Twitter' },
+  { href: 'mailto:xjoylu@gmail.com', label: 'Email' },
+  { href: '/rss.xml', label: 'RSS' },
+]
+
+export const ICON_MAP: IconMap = {
+  Website: 'lucide:globe',
+  GitHub: 'lucide:github',
+  LinkedIn: 'lucide:linkedin',
+  Twitter: 'lucide:twitter',
+  Email: 'lucide:mail',
+  RSS: 'lucide:rss',
+}
