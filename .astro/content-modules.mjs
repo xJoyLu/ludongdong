cdc73b9a05cdffaac17ebff0cbd9eca0c7d05bf4
卢东东 <xjoylu@gmail.com>
2025-07-31 04:56:04 +0800

    up blog
    更新《一句话记账机器人，用飞书 + 多维表格撸一个最小MVP》

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -4,6 +4,8 @@ export default new Map([
 ["src/content/blog/2023-post/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2F2023-post%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/2024-post/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2F2024-post%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/Deploy-Discourse/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2FDeploy-Discourse%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/blog/lu-tools-2024/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Flu-tools-2024%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/issue-7/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fissue-7%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/blog/lu-tools-2024/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Flu-tools-2024%2Findex.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/feishu_bookkeeping_bot/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Ffeishu_bookkeeping_bot%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/blog/feishu_bookkeeping_bot/feishu_bot.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Ffeishu_bookkeeping_bot%2Ffeishu_bot.mdx&astroContentModuleFlag=true")]]);
 		
