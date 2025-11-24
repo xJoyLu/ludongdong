7160831c49f1322b76ce38291bce68ec8a24d67f
卢东东 <xjoylu@gmail.com>
2025-11-25 07:02:17 +0800

    1

--- astro.config.ts
+++ astro.config.ts
@@ -20,7 +20,7 @@ import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  site: 'https://astro-erudite.vercel.app',
+  site: 'https://ldd.cc',
   integrations: [
     expressiveCode({
       themes: ['github-light', 'github-dark'],
